c7d75c7a3d22fbe76d9164ed67d68004707e0c3c
elgs <elgs1980@hotmail.com>
2014-09-10 14:58:59 +0800

    Updated w1 client.

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@
 (function () {
     "use strict";
 
+    var headers = {
+        api_token_id: 'elgs@netnamed.net',
+        api_token_key: '837bc77c-3514-11e4-b6bd-0401128c2501'
+    };
+
     var app = angular.module('app', ['jqDirective']);
     app.controller('ListController', ['$scope', '$http', function ($scope, $http) {
-        var headers = {
-            api_token_id: 'elgs@netnamed.net',
-            api_token_key: '837bc77c-3514-11e4-b6bd-0401128c2501'
-        };
         $http.get('https://netnamed.com:7443/api/secret', {
             headers: headers
         }).success(function (data, status, headers, config) {
@@ -19,15 +20,43 @@
             console.log(data);
         });
 
+        $scope.submit = function (id, username, password) {
+            console.log(id, username, password);
+            $http.put('https://netnamed.com:7443/api/secret?meta=1', {
+                id: id,
+                client: username,
+                secret: password,
+                ip_addresses: "*",
+                server: 'pptpd',
+                status: "0"
+            }, {
+                headers: headers
+            }).success(function (data, status, headers, config) {
+                console.log(data);
+                for (var i in $scope.list) {
+                    if ($scope.list[i].id === id) {
+                        $scope.list[i].class = "alert alert-success";
+                    }
+                }
+            }).error(function (data, status, headers, config) {
+                console.log(data);
+            });
+        };
+
     }]);
 
     app.directive('mainHeader', function () {
         return {
             restrict: 'E',
-            controller: function ($scope, $http) {
+            controller: function ($scope) {
             },
             templateUrl: 'html/main-header.html'
         };
+    });
+
+    $('#accordion').on('shown.bs.collapse', function (e) {
+        var input = $($(e.target).find('form')[0]).find('input')[0];
+        input.focus(500);
     });
 })();
 
